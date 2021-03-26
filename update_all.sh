f5138bf4e82c1674a331711965e4c8506eb125d1
Pascal Hein <pascal.hein@gmx.de>
2021-03-26 08:40:08 +0100

    changing print

--- update_all.sh
+++ update_all.sh
@@ -3,7 +3,7 @@ prefix="$(basename "$0"):"
 # iterate over all dicts in cwd -> skript must be started from aur dir
 for dir in */; do
     cd $dir
-    echo "$prefix working on $dir"
+    echo "$prefix checking $dir"
     # update info about remote status
     feedback="$(git remote -v update 2>&1 > /dev/null)"
 
